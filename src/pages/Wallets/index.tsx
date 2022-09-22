038b7e95f7274c618fc0bec57e18944a0a311f06
Jonas Vargaski <jonas.vargaski@hiplatform.com>
2022-09-22 12:26:20 -0300

    add emptystate

--- src/pages/Wallets/index.tsx
+++ src/pages/Wallets/index.tsx
@@ -1,12 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import { useWallets } from '~/hooks/resources/useWallets';
 import { useDeleteWallet } from '~/hooks/resources/useDeleteWallet';
+import FinancialData from '../../assets/financial_data.svg';
 
 import { Flex } from '~/components/Flex';
 import { Typography } from '~/components/Typography';
 
-import { List } from './styles';
+import { Actions, Container, EmptyState, List } from './styles';
 import { WalletItem } from './WalletItem';
+import { Button } from '@mui/material';
 
 export function Wallets() {
   const navigate = useNavigate();
@@ -14,22 +16,38 @@ export function Wallets() {
   const deleteWallet = useDeleteWallet();
 
   return (
-    <>
+    <Container>
       <Flex m='8px 0 20px'>
         <Typography variant='pageTitle'>Carteiras</Typography>
       </Flex>
 
-      <List>
-        {wallets?.map((wallet) => (
-          <WalletItem
-            key={wallet.id}
-            wallet={wallet}
-            onExclude={() => deleteWallet.mutate(wallet.id)}
-            onView={() => setTimeout(() => navigate(`/wallets/performance/${wallet.id}`))}
-            onEdit={() => setTimeout(() => navigate(`/wallets/manage/${wallet.id}`))}
-          />
-        ))}
-      </List>
-    </>
+      {wallets?.length ? (
+        <List>
+          {wallets?.map((wallet) => (
+            <WalletItem
+              key={wallet.id}
+              wallet={wallet}
+              onExclude={() => deleteWallet.mutate(wallet.id)}
+              onView={() => setTimeout(() => navigate(`/wallets/performance/${wallet.id}`))}
+              onEdit={() => setTimeout(() => navigate(`/wallets/manage/${wallet.id}`))}
+            />
+          ))}
+        </List>
+      ) : (
+        <EmptyState>
+          <img src={FinancialData} alt='EmptyState' />
+          <div>
+            <Typography variant='description'>Nenhuma carteira encontrada.</Typography>
+            <Typography variant='description'>Cadastre e gerencie suas carteiras!</Typography>
+          </div>
+        </EmptyState>
+      )}
+
+      <Actions>
+        <Button variant='contained' onClick={() => navigate('/wallets/manage')}>
+          Criar Carteira
+        </Button>
+      </Actions>
+    </Container>
   );
 }
--- src/pages/Wallets/styles.ts
+++ src/pages/Wallets/styles.ts
@@ -1,8 +1,41 @@
 import styled from '@emotion/styled';
+import { Card } from '~/components/Generic';
+
+export const Container = styled.div`
+  height: 100%;
+  overflow-y: auto;
+  position: relative;
+  padding-bottom: 70px;
+`;
+
+export const EmptyState = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+  gap: 20px;
+  width: 100%;
+  text-align: center;
+
+  img {
+    max-width: 35%;
+  }
+`;
 
 export const List = styled.ul`
   display: grid;
   grid-gap: 1.3rem;
   grid-template-columns: 1fr;
   grid-auto-rows: auto;
+`;
+
+export const Actions = styled(Card)`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
